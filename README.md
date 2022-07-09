7ef8a96f0fe17be8cc3ad065c606bd1110191d96
Kurt Schuster <qrt@qland.de>
2022-07-09 15:14:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# **padSel**
+### **Column Homogenous Comment Positions for vsCode**
+
+<br>
+
+**Because of the lack of a real column edit function in vsCode there is some typework to be saved with this script**
+
+- select some lines, be careful to select complete lines
+- the line with the most right comment is used as position template  
+  if no existing comments are found a default position is used  
+  already commented or longer lines are ignored
+- press your binded key or run multiCommand.padSel (with command palette)
+- the selection will be processed and replaced
+
+<br>
+
+<img src="image/padSel.gif" width=480>
+
+<br>
+
+### **settings.json**
+``` 
+{
+    "multiCommand.commands": [
+        {
+            "command": "multiCommand.padSel",
+            "interval": 250,                            // higher for slow machines
+            "sequence": [
+                "editor.action.clipboardCopyAction",
+                {
+                    "command": "command-runner.run",
+                    "args": {
+                        "command": "padSel"
+                    }
+                },
+                "workbench.action.focusActiveEditorGroup",
+                "editor.action.clipboardPasteAction"
+            ]
+        }
+    ],
+
+    "command-runner.terminal.name": "runCommand",
+    "command-runner.terminal.autoClear": true,
+    "command-runner.terminal.autoFocus": false,         // if true, interval in multiCommand may be higher
+    "command-runner.commands": {
+        "padSel": "C:\\..\\python.exe C:\\..\\padSel.py ${fileExtname}"
+    }
+}
+```
+
+### **optional keybindings.json**
+```
+[
+    {
+        "key": "ctrl+alt+p",
+        "command": "multiCommand.padSel",
+        "when": "editorTextFocus"
+    }
+]
+```
+
+### **needed extensions**
+https://marketplace.visualstudio.com/items?itemName=ryuta46.multi-command  
+https://marketplace.visualstudio.com/items?itemName=edonet.vscode-command-runner
+
+<br>
+
+#### V0.5 220709
+---
+
+#### contact
+[qrt@qland.de](mailto:qrt@qland.de)
---
+++ script/padSel.py
@@ -0,0 +1,44 @@
+# pad selection V0.5 qrt@qland.de 220709
+# Column Homogenous Comment Positions for vsCode
+# 
+# - select some lines, be careful to select complete lines
+# - the line with the most right comment is used as position template  
+#   if no existing comments are found a default position is used  
+#   already commented or longer lines are ignored
+# - press your binded key or run multiCommand.padSel (with command palette)
+# - the selection will be processed and replaced
+#
+# needed extensions
+# https://marketplace.visualstudio.com/items?itemName=ryuta46.multi-command
+# https://marketplace.visualstudio.com/items?itemName=edonet.vscode-command-runner
+
+import sys
+import pyperclip as clip
+from operator import itemgetter
+
+# extension: comment string dictonary
+extD = { '.c': '//', '.cpp': '//', '.h': '//', '.py': '#', '.asm': ';' }
+
+copDef = 64                                                     # comment position default
+
+ext = sys.argv[1]                                               # file extension
+cms = extD.get(ext, None)                                       # comment string for extension
+
+if not cms:                                                     # extension not found
+    print('error: extension not found')                         #
+    exit(0)                                                     #
+
+spl = clip.paste().splitlines()                                 # get clipboard and split lines
+lines = [ [line.rstrip(), line.rfind(cms)] for line in spl ]    # [ [line, comment position] ]
+
+cop = max(lines, key=itemgetter(1))[1]                          # comment position
+                                                                #
+if cop == -1:                                                   # 
+    cop = copDef                                                #
+
+for line in lines:                                              # pad lines
+    if line[1]==-1 and len(line[0])<cop:                        #
+        line[0] = ('{:<' + str(cop) + '}{}').format(line[0], cms)
+
+nLines = '\n'.join([ line[0] for line in lines ]) + '\n'        # join lines
+clip.copy(nLines)                                               # past to clipboard
